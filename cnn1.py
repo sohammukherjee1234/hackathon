dd9be94eb49c4f80891abce36713fa5cd7d30281
sohammukherjee1234 <rockstarsam1234@gmail.com>
2018-03-22 20:40:44 +0530

    cnn ready

--- cnn1.py
+++ cnn1.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
+Created on Thu Mar 22 19:34:00 2018
+
+@author: soham
+"""
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
 Created on Thu Mar 15 22:20:01 2018
 
 @author: soham
@@ -43,6 +50,27 @@ val_data=datasets.ImageFolder(val_path,transform)
 
 train_loader=DataLoader(train_data,batch_size=4,shuffle=True,num_workers=1)
 val_loader=DataLoader(val_data,batch_size=4,shuffle=True,num_workers=1)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net,self).__init__()
+        self.conv1=nn.Conv2d(3,8,5,padding=2)
+        self.pool=nn.MaxPool2d(2,2)
+        self.conv2=nn.Conv2d(8,16,5,padding=2)
+        self.conv3=nn.Conv2d(16,32,5,padding=2)
+        self.fc1=nn.Linear(32*28*28,120)
+        self.fc2=nn.Linear(120,14)
+    
+    def forward(self,x):
+        x=self.pool(F.relu(self.conv1(x)))
+        x=self.pool(F.relu(self.conv2(x)))
+        x=self.pool(F.relu(self.conv3(x)))
+        x=x.view(-1,32*28*28)
+        x=self.fc1(x)
+        x=self.fc2(x)
+        return x
+        
+        
 
 
         
@@ -96,4 +124,5 @@ for epoch in range(5):
     plt.savefig('plt.png')
     plt.show()
 
-        
+
+torch.save(net,'model.pth')
